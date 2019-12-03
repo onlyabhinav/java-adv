a47e8b10142d5b78114932a66dc03957172d0e65
onlyabhinav <dev.abhinav23@gmail.com>
2019-12-03 22:27:43 +0530

    sunrise to git

---
+++ sunrise/src/main/java/dev/onlyabhinav/sunrise/SunriseApplication.java
@@ -0,0 +1,13 @@
+package dev.onlyabhinav.sunrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseApplication.class, args);
+	}
+
+}
---
+++ sunrise/src/test/java/dev/onlyabhinav/sunrise/SunriseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.onlyabhinav.sunrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
